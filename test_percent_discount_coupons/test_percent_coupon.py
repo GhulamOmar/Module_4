0e27ab46e3bb7c478c40db2342e53861089b3087
Omar <gomar@dmacc.edu>
2019-09-17 08:29:50 -0700

    fourth test

--- test_percent_discount_coupons/test_percent_coupon.py
+++ test_percent_discount_coupons/test_percent_coupon.py
@@ -6,6 +6,7 @@ class MyTestCase(unittest.TestCase):
        self.assertAlmostEquals(17.12,percent_coupon.calculate_order(20))
        self.assertAlmostEquals(18.604, percent_coupon.calculate_order(22))
        self.assertAlmostEquals(24.54, percent_coupon.calculate_order(30))
+       self.assertAlmostEquals(22, percent_coupon.calculate_order(33))
 
 
 if __name__ == '__main__':

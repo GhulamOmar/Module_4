1c17afe08dee1f5433f64b65ecf492a320268c9f
Omar <gomar@dmacc.edu>
2019-09-17 08:49:18 -0700

    Final initial of nested if statement Assignment

--- test_percent_discount_coupons/test_percent_coupon.py
+++ test_percent_discount_coupons/test_percent_coupon.py
@@ -1,12 +1,20 @@
-import unittest
+"""
+Program: test_percent_coupon.py
+Author: Ghulam Omar
+Last date modified: 09/17/19
 
+this program is a test file for percent_coupon.py.
+"""
+import unittest
 from percent_discount_coupons import percent_coupon
+
+
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-       self.assertAlmostEquals(17.12,percent_coupon.calculate_order(20))
-       self.assertAlmostEquals(18.604, percent_coupon.calculate_order(22))
-       self.assertAlmostEquals(24.54, percent_coupon.calculate_order(30))
-       self.assertAlmostEquals(43.09, percent_coupon.calculate_order(55))
+        self.assertAlmostEqual(17.12, percent_coupon.calculate_order(20))  # first test
+        self.assertAlmostEquals(23.798, percent_coupon.calculate_order(29))  # second test
+        self.assertAlmostEquals(24.54, percent_coupon.calculate_order(30))  # third test
+        self.assertAlmostEquals(46.80, percent_coupon.calculate_order(60))  # fourth test
 
 
 if __name__ == '__main__':

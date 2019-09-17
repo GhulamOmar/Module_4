641b5fc133848f5aeb67f5e13df4475a568c657a
Omar <gomar@dmacc.edu>
2019-09-17 08:25:15 -0700

    second test passed

--- test_percent_discount_coupons/test_percent_coupon.py
+++ test_percent_discount_coupons/test_percent_coupon.py
@@ -4,7 +4,7 @@ from percent_discount_coupons import percent_coupon
 class MyTestCase(unittest.TestCase):
     def test_something(self):
        self.assertAlmostEquals(17.12,percent_coupon.calculate_order(20))
-       self.assertAlmostEquals(23.798, percent_coupon.calculate_order(29))
+       self.assertAlmostEquals(18.604, percent_coupon.calculate_order(22))
 
 
 if __name__ == '__main__':
